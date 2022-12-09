29b320427b0b08d256a646f2c98259e1afbae321
paruma <paruma184az@gmail.com>
2022-12-09 22:35:31 +0900

    step18 実装

--- net.c
+++ net.c
@@ -10,6 +10,7 @@
 #include "net.h"
 #include "ip.h"
 #include "icmp.h"
+#include "udp.h"
 
 struct net_protocol {
     struct net_protocol *next;
@@ -321,6 +322,10 @@ net_init(void)
     }
     if (icmp_init() == -1) {
         errorf("icmp_init() failure");
+        return -1;
+    }
+    if (udp_init() == -1) {
+        errorf("udp_init() failure");
         return -1;
     }
     infof("initialized");
